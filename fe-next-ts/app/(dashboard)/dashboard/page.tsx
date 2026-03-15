077a8d0c7fe136889fc26fc3253950831d7f68b2
nflnsr <naufaln176@gmail.com>
2026-03-15 16:17:44 +0700

    style: add greeting to user

--- fe-next-ts/app/(dashboard)/dashboard/page.tsx
+++ fe-next-ts/app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import * as React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { DashboardTable } from "./_components/dashboard-table";
+import { useAuthStore } from "@/stores/auth";
 
 export default function Dashboard() {
+  const { user } = useAuthStore();
   return (
-    <div className="box-border">
-      <div className="max-w-5xl mx-auto flex flex-col min-h-screen justify-center pb-24">
+    <div className="box-border max-w-5xl mx-auto flex flex-col min-h-screen justify-center gap-8 pb-16">
+      <div className="px-2 text-center">Hi, {user?.name}. Welcome 👋🏻</div>
+      <div className="">
         <div className="w-full max-w-full overflow-x-auto">
           <DashboardTable />
         </div>
