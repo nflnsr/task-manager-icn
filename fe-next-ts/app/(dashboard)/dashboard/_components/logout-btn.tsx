5ffd4681e8ad304d9b1046615f9f9e0f58d1f1cc
nflnsr <naufaln176@gmail.com>
2026-03-15 19:21:38 +0700

    fix: remove push router

--- fe-next-ts/app/(dashboard)/dashboard/_components/logout-btn.tsx
+++ fe-next-ts/app/(dashboard)/dashboard/_components/logout-btn.tsx
@@ -3,16 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { clearRefreshToken } from "@/server/auth";
 import { useAuthStore } from "@/stores/auth";
-import { useRouter } from "next/navigation";
 
 export function LogoutBtn() {
-  const route = useRouter();
   const { clearAuthStore } = useAuthStore();
 
   const handleLogout = async () => {
     await clearRefreshToken();
     clearAuthStore();
-    route.push("/login");
   };
 
   return (
